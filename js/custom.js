ac0130aa6cc07e80d3eb69297709e79d978eb680
abbat76 <sergiy.kurkin@gmail.com>
2021-04-13 18:40:53 +0300

    Сайт создан и запущен
    ЗАЛИВКА последней рабочей версии
    v 1.0.0
    вертится на хостинге

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	var headerHeight = $('header').outerHeight();
+
+	$('.slide-section').click(function(e) {
+
+		var linkHref=$(this).attr('href');
+
+		$('html, body').animate({
+			scrollTop: $(linkHref).offset().top - headerHeight
+		},1000);
+
+		e.preventDefault();
+	});
+
+});
